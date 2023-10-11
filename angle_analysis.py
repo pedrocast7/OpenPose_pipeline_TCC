175d7f32936ed67c3a04806ad805bdf567b12190
Pedro Castro <[pedrocast77@gmail.com]>
2023-10-11 09:57:36 -0300

    fix:angle_analysis_pipeline

--- angle_analysis.py
+++ angle_analysis.py
@@ -9,34 +9,50 @@ from scipy import interpolate
 from scipy import stats
 from mpl_point_clicker import clicker
 from Openpose_lib_functions import scale_and_offset, lowpassfilter, select_signals_area
+from sklearn.metrics import mean_squared_error
 
-
-def scale_and_offset(input_signal: np.array) -> np.array:
-
-  # falta documentar a função
-  output_sig = input_signal - np.mean(input_signal) # retira offset
-  output =  output_sig/np.max(np.abs(output_sig)) # normaliza amplitude
-  return output
 
 
 columns1 = ["joint_angles_lemoh"]
-df = pd.read_csv('/home/lamic/Openpose-pedro/scripts_openpose/codigos/video-01_angles_lemoh.csv', usecols=columns1)
+df = pd.read_csv('C:/Users/pedro/OneDrive/Documentos/UFPA - Material/TCC STUFF/lamic/Samples_04-10/DATA_TXT_CSV/LB_angle_jessyka_abducao_lat.csv')
 
 columns2 = ["joint_angles"]
-df2 = pd.read_csv('/home/lamic/Openpose-pedro/scripts_openpose/codigos/test_angle.csv', usecols=columns2)
+df2 = pd.read_csv('C:/Users/pedro/OneDrive/Documentos/UFPA - Material/TCC STUFF/lamic/Samples_04-10/DATA_TXT_CSV/OP_angle_jessyka_abducao_lat.csv')
 
 print("Contents in csv file:\n", df)
 
+lb = pd.read_table('C:/Users/pedro/OneDrive/Documentos/UFPA - Material/TCC STUFF/lamic/Samples_04-10/DATA_TXT_CSV/jessica abdução lateral KINEM.txt',
+                    decimal = '.',
+                      encoding='latin-1')
 
 df = df[1:]
 df2 = df2[1:]
 
 lemoh = df.to_numpy()
-openpose = df2.to_numpy()
+openpose = df2.to_numpy() # shape(n_frames, 1)
 
-x_lemoh = np.arange( len(lemoh) )
+## Turn to 1D vec
+lemoh = lemoh.reshape(-1)
+openpose = openpose.reshape(-1)
 
-x_openpose_raw = np.arange( len(openpose) )
+# Define vetores com informação temporal
+
+# Define o vetor tempo de referência (a partir dos dados do lemoh)
+time_vec_lb = np.array(lb['Time'])
+
+# Define o vetor de tempo dos dados vindos do openpose
+time_vec_op = np.linspace(time_vec_lb.min(), time_vec_lb.max(), len(openpose))
+
+f = interpolate.interp1d(time_vec_op,  openpose, 
+                         kind= 'next') # calcula função de interpolação
+openpose = f(time_vec_lb)
+
+#openpose = scale_and_offset(openpose, 'n')
+#lemoh = scale_and_offset(lemoh, 'n')
+
+#time_vec_lb = np.arange( len(lemoh) )
+
+#time_vec_op = np.arange( len(openpose) )
 
 
 lhminf, lhmsup, opinf, opsup = select_signals_area(lemoh, openpose)
@@ -48,35 +64,11 @@ index_inf_op = int(np.round(opinf[0])) # índice inferior do vetor openpose
 index_sup_op = int(np.round(opsup[0])) # índice super do vetor openpose
 
 
-## Frequency domain analysis
 
-
-# freq_a = 120 # frequência de amostragem das câmeras do LEMoH (em Hertz)
-# N = 2**14 # número de pontos de frequência
-# f, op_right_hip_x_fft = signal.freqz(openpose,worN=N, fs=freq_a)
-# f, lb_right_hip_x_fft = signal.freqz(lemoh,worN=N, fs=freq_a)
-
-
-# plt.figure()
-# # plt.figure(figsize=(1*6.4,1*4.8)) # inicia nova figura e ajusta tamanho
-# plt.plot(f,10*np.log10(np.abs(lb_right_hip_x_fft)), 'r', 
-#          label = 'LEMoH') # traça gráfico
-# plt.plot(f, 10*np.log10(np.abs(op_right_hip_x_fft)), 'b', 
-#          label ='OpenPose') # traça gráfico
-# plt.grid('True') # ativa grid
-# plt.xlabel('Frequency [Hz]') # legenda do eixo horizontal
-# plt.ylabel('Magnitude of the Fourier transform [dB]') # legenda do eixo vertical
-# plt.title('Hip - component x') # título do gráfico
-# plt.axis([0, 60, -10, 40])
-# plt.legend() # exibe legenda
-# plt.show()
-
-
-x_lemoh = np.arange( len(lemoh) )
 
 # Corta os vetores de sinal e tempo
 lemoh_trimmed = np.array(lemoh[index_inf_lb:index_sup_lb+1])
-x_lemoh_trimmed = np.linspace(x_lemoh[index_inf_lb], x_lemoh[index_sup_lb],
+x_lemoh_trimmed = np.linspace(time_vec_lb[index_inf_lb], time_vec_lb[index_sup_lb],
                                   index_sup_lb-index_inf_lb+1) 
 
 openpose_trimmed = np.array(openpose[index_inf_op:index_sup_op+1])
@@ -85,11 +77,14 @@ x_openpose_trimmed = np.linspace(x_lemoh_trimmed.min(), x_lemoh_trimmed.max(),
 
 
 
-x_openpose = np.arange( len(openpose) )
-x_openpose = np.interp(x_openpose, (min(x_openpose), max(x_openpose)), (min(x_lemoh), max(x_lemoh)) )
 
-openpose_trimmed = openpose_trimmed - (np.mean(openpose_trimmed) - np.mean(lemoh_trimmed))
-x_openpose_trimmed = x_openpose_trimmed - (np.mean(x_openpose_trimmed) - np.mean(x_lemoh_trimmed))  #offset adjust
+
+#x_openpose = np.arange( len(openpose) )
+#x_openpose = np.interp(x_openpose, (min(x_openpose), max(x_openpose)), (min(time_vec_lb), max(time_vec_lb)) )
+
+#openpose_trimmed = openpose_trimmed - (np.mean(openpose_trimmed) - np.mean(lemoh_trimmed))
+#x_openpose_trimmed = x_openpose_trimmed - (np.mean(x_openpose_trimmed) - np.mean(x_lemoh_trimmed))  #offset adjust
+
 
 #plt.plot(x_openpose_trimmed[index_inf_op], openpose_trimmed[index_inf_op], 'bo')
 #plt.plot(x_openpose_trimmed[index_sup_op], openpose_trimmed[index_sup_op], 'bo')
@@ -98,40 +93,49 @@ plt.figure()
 plt.plot(x_lemoh_trimmed, lemoh_trimmed, label="LEMOH")
 plt.plot(x_openpose_trimmed, openpose_trimmed, label="Openpose")
 plt.title("LEMOH x Openpose joint angle")
-plt.xlabel("Samples")
+plt.xlabel("Time (s)")
 plt.ylabel("Estimated angle")
+plt.grid()
 plt.show()
 
-openpose_trimmed = np.resize(openpose_trimmed,lemoh_trimmed.size)
+#openpose_trimmed = np.resize(openpose_trimmed,lemoh_trimmed.size)
+
+
+### Fill NaN values that may be lost after trim
+openpose_trimmed = openpose_trimmed[~np.isnan(openpose_trimmed)]
 
 f = interpolate.interp1d(np.arange(0, len(openpose_trimmed)), openpose_trimmed)
-stretched_openp = f(np.linspace(0.0, len(openpose_trimmed)-1, len(lemoh_trimmed)))
+openpose_trimmed = f(np.linspace(0.0, len(openpose_trimmed)-1, len(lemoh_trimmed)))
 
-
+#f = interpolate.interp1d(openpose_trimmed, 
+#                         kind= 'next') # calcula função de interpolação
+#openpose_trimmed = f(x_lemoh_trimmed)
 
 
 
 # Calcula sinal de erro
-err = np.subtract(lemoh_trimmed,stretched_openp)
-err = np.resize(err, (1, len(lemoh_trimmed)))
-err = np.transpose(err)
+err = lemoh_trimmed - openpose_trimmed
+#err = np.resize(err, (1, len(lemoh_trimmed)))
+err = err.reshape(-1) #1d
 
 # Calcula SNR signal-to-noise ratio
-mse = np.mean(err ** 2) # mean square error
+mse = mean_squared_error(lemoh_trimmed, openpose_trimmed) # mean square error
 signal_e = np.mean(lemoh_trimmed ** 2) # signal energy
-SNR = 10 * np.log10(signal_e/mse)
+SignalNR = 10 * np.log10(signal_e/mse)
 
-print('The signal-noise ratio is: ', SNR, ' dB')
+print('The signal-noise ratio is: ', SignalNR, ' dB')
 
 
-lemoh_trimmed_graph = scale_and_offset(lemoh_trimmed)
-stretched_openp_graph = scale_and_offset(stretched_openp)
-err_graph = scale_and_offset(err)
+#lemoh_trimmed = scale_and_offset(lemoh_trimmed, 'n')
+#openpose_trimmed = openpose_trimmed - (np.mean(openpose_trimmed) - np.mean(lemoh_trimmed))
+#openpose_trimmed = scale_and_offset(openpose_trimmed, 'n')
+
+err_plot = err + np.mean(lemoh_trimmed) ## offset adjust for plotting
 
 plt.figure()
-plt.plot(x_lemoh_trimmed, lemoh_trimmed_graph,'g', label="LEMOH")
-plt.plot(x_openpose_trimmed, stretched_openp_graph[0:len(x_openpose_trimmed)],'b', label="Openpose")
-plt.plot(x_lemoh_trimmed, err_graph,'r',label='Error')
+plt.plot(x_lemoh_trimmed, lemoh_trimmed,'g', label="LEMOH")
+plt.plot(x_lemoh_trimmed, openpose_trimmed,'b', label="Openpose")
+plt.plot(x_lemoh_trimmed, err_plot, 'r',label='Error')
 plt.title("Error analysis")
 plt.xlabel("Samples")
 plt.ylabel("Estimated angle")
@@ -142,15 +146,15 @@ plt.show()
 
 
 
-#filtered_openpose_15 = lowpassfilter(stretched_openp[0:len(x_openpose_trimmed)], thresh=0.15)
-#filtered_openpose_20 = lowpassfilter(stretched_openp[0:len(x_openpose_trimmed)], thresh=0.20)
-filtered_openpose_25 = lowpassfilter(stretched_openp[0:len(x_openpose_trimmed)], thresh=0.25)
-#filtered_openpose_30 = lowpassfilter(stretched_openp[0:len(x_openpose_trimmed)], thresh=0.30)
-#filtered_openpose_35 = lowpassfilter(stretched_openp[0:len(x_openpose_trimmed)], thresh=0.35)
+#filtered_openpose_15 = lowpassfilter(openpose_trimmed[0:len(x_openpose_trimmed)], thresh=0.15)
+#filtered_openpose_20 = lowpassfilter(openpose_trimmed[0:len(x_openpose_trimmed)], thresh=0.20)
+filtered_openpose_25 = lowpassfilter(openpose_trimmed, thresh=0.25)
+#filtered_openpose_30 = lowpassfilter(openpose_trimmed[0:len(x_openpose_trimmed)], thresh=0.30)
+#filtered_openpose_35 = lowpassfilter(openpose_trimmed[0:len(x_openpose_trimmed)], thresh=0.35)
 
 
 # Savgol Filtering
-#filtered_openpose = signal.savgol_filter(stretched_openp[0:len(x_openpose_trimmed)], 51,2)
+#filtered_openpose = signal.savgol_filter(openpose_trimmed[0:len(x_openpose_trimmed)], 51,2)
 
 ## Threshold value selection 0.25
 # plt.figure()
@@ -190,7 +194,7 @@ print('The signal-noise ratio, after the filtering process, is: ', SNR2, ' dB')
 
 plt.figure()
 plt.plot(x_lemoh_trimmed, lemoh_trimmed,'g', label="LEMOH")
-plt.plot(x_openpose_trimmed, filtered_openpose_25[0:len(x_openpose_trimmed)],'b', label="Openpose")
+plt.plot(x_lemoh_trimmed, filtered_openpose_25,'b', label="Openpose")
 plt.title("Filtered Openpose")
 plt.xlabel("Samples")
 plt.ylabel("Estimated angle")
@@ -205,7 +209,7 @@ arr2_interp = interpolate.interp1d(np.arange(filtered_openpose_25.size),filtered
 openpose2_stretch = arr2_interp(np.linspace(0,filtered_openpose_25.size-1,lemoh_trimmed.size))
 
 # f = interpolate.interp1d(np.arange(0, len(filtered_openpose_25)), filtered_openpose_25)
-# stretched_openp = f(np.linspace(0.0, len(filtered_openpose_25)-1, len(lemoh_trimmed)))
+# openpose_trimmed = f(np.linspace(0.0, len(filtered_openpose_25)-1, len(lemoh_trimmed)))
 
 ##find line of best fit
 a_fit, b_fit = np.polyfit(openpose2_stretch, lemoh_trimmed, 1)
@@ -220,7 +224,7 @@ plt.grid('True')
 plt.xlabel('OpenPose inference')
 plt.ylabel('LEMOH inference')
 plt.title('Angle estimation')
-plt.savefig('cloud_OpLh_Angle.eps', format='eps')
+#plt.savefig('cloud_OpLh_Angle.eps', format='eps')
 plt.show()
 
 ## calculating cross corelation and Pearson's corrcoef
@@ -233,7 +237,7 @@ plt.figure()
 plt.plot(Pear_coef, color='red', linestyle='--')
 plt.grid('True')
 plt.title('Angle Pearson\'s Correlation = ' + "{:.4f}".format(Pear_coef[0]))
-plt.savefig('Pearsons_correlation_OpLh_Angle.eps', format='eps')
+#plt.savefig('Pearsons_correlation_OpLh_Angle.eps', format='eps')
 plt.show()
 
 
@@ -244,7 +248,7 @@ plt.grid('True')
 plt.title('Angle Cross-correlation')
 plt.xlabel('Lags')
 plt.ylabel('Normalized Cross-correlation')
-plt.savefig('Cross_correlation_OpLh_Angle.eps', format='eps')
+#plt.savefig('Cross_correlation_OpLh_Angle.eps', format='eps')
 plt.show()
 
 lags = x_corr[0]
